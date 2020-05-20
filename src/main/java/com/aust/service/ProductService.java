188df21e332bb4408820ca78b3f741cb3a2dc6ee
zhoun <172906012@qq.com>
2020-05-20 13:34:13 +0800

    dubbo

---
+++ src/main/java/com/aust/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.aust.service;/*
+ * Copyright © 2016 睿泰集团 版权所有
+ */
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Autor zhouNan
+ * @Date 2019/10/11 10:21
+ * @Description ProductService
+ **/
+@Service
+public interface ProductService {
+    String getName(String name);
+}
